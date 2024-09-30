331d3248e6e0b975ba6995899199d4b40cf9154d
jules <62078259+juleswhi@users.noreply.github.com>
2024-09-30 14:48:09 +0100

    Update MecanumMovementTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveropmodes/MecanumMovementTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveropmodes/MecanumMovementTeleOp.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Mecanum;
+import static org.firstinspires.ftc.teamcode.Helper.*;
 import org.firstinspires.ftc.teamcode.hardwaremaps.DeepHardwareMap;
 
 /**
