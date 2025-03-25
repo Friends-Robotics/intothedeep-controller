2c579615e5c7dbfc8694af858a68f6f5bd9bec65
Matthew Gracey <m.gracey1409@gmail.com>
2025-03-25 13:47:49 +0000

    Add arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/Arm.java
@@ -1,30 +1,35 @@
 package org.firstinspires.ftc.teamcode.helpers;
 import com.qualcomm.robotcore.hardware.Servo;
 public class Arm {
-    private Servo RightExtendServo;
-    private Servo LeftExtendServo;
-    public Claw Claw;
+    private final Servo RightExtendServo;
+    private final Servo LeftExtendServo;
+    public final Claw Claw;
     private boolean isExtended;
-    public Arm(Servo RES, Servo LES, Servo RAS, Servo LAS, Servo CLS)
-    {
-        RightExtendServo = RES;
-        LeftExtendServo = LES;
-        Claw = new Claw(RAS,LAS,CLS);
+
+    public Arm(Servo rightExtensionServo, Servo leftExtensionServo,
+               Servo rightArmServo, Servo leftArmServo, Servo clawServo) {
+        RightExtendServo = rightExtensionServo;
+        LeftExtendServo = leftExtensionServo;
+        Claw = new Claw(rightArmServo,leftArmServo,clawServo);
         isExtended = false;
         RightExtendServo.setPosition(0);
         LeftExtendServo.setPosition(0);
     }
 
-    public void ToggleExtension()
-    {
+    public void ToggleExtension() {
         if(!isExtended){
             RightExtendServo.setPosition(0.4);
             LeftExtendServo.setPosition(0.4);
             isExtended = true;
-        }else{
+        }
+        else {
             RightExtendServo.setPosition(0);
             LeftExtendServo.setPosition(0);
             isExtended = false;
         }
+    }
+
+    public Claw getClaw() {
+        return Claw;
     }
 }
